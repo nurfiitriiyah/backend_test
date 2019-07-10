985a0adffaf3a73361d2f0fc8112d0e5c4eb0f5c
nurfiiitriiiyah <nurfiitriiyah@gmail.com>
2019-07-10 12:06:06 +0700

    all done

--- routes/api.js
+++ routes/api.js
@@ -8,10 +8,22 @@ router.get('/getDatas', async function (req, res, next) {
         var access_token = "nnnnn";
         var feeds = await unirest.get('https://graph.facebook.com/v3.3/me/feed?fields=application&access_token=' + access_token)
         var data = (JSON.parse(feeds.body));
+
+
         if (data.data.length < 1) {
             res.json({status: "ok", message: "Not Found"})
         }else{
-            res.json(data.data[paginate])
+            var allDatas = []
+            var singleDatas = []
+            data.data.forEach(function (val,key) {
+                if(singleDatas.length <= 10 ){
+                    singleDatas.push(val)
+                }else{
+                    allDatas.push(singleDatas)
+                    singleDatas = []
+                }
+            })
+            res.json(allDatas[paginate])
         }
     } else {
         res.status(500).json({
